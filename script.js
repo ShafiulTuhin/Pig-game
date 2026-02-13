0c87472c205ddd8041c6861d6839cd25ca9a8b3b
ShafiulTuhin <tuhin6488@gmail.com>
2026-02-13 18:27:05 +0600

    completed all functions

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 //Selecting elements
-const playerOne = document.querySelector("#player--0");
-const playerTwo = document.querySelector("#player--1");
-const scorePlayerOne = document.querySelector(".current_score--0");
-const scorePlayerTwo = document.querySelector(".current_score--1");
-const playerOneTotal = document.querySelector("#player_0--total");
-const playerTwoTotal = document.querySelector("#player_1--total");
+const playerOne = document.querySelector("#player--1");
+const playerTwo = document.querySelector("#player--2");
+const playerOneName = document.querySelector(".name--1");
+const playerTwoName = document.querySelector(".name--2");
+const scorePlayerOne = document.querySelector(".current_score--1");
+const scorePlayerTwo = document.querySelector(".current_score--2");
+const playerOneTotal = document.querySelector("#player_1--total");
+const playerTwoTotal = document.querySelector("#player_2--total");
 const dice = document.querySelector(".dice");
 const rollDiceBtn = document.querySelector(".rollDice");
 const newGameBtn = document.querySelector(".newGame");
 const holdBtn = document.querySelector(".hold");
 
 //Initial conditions
-playerOneTotal.textContent = 0;
-playerTwoTotal.textContent = 0;
 let currentScore = 0;
-console.log(currentScore);
-let activePlayer = 0;
-let TotalScore = 0;
+let activePlayer = 1;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
+//Switch player
+const switchPlayer = () => {
+  //Current score zero
+  document.querySelector(`.current_score--${activePlayer}`).textContent = 0;
+  //Switching player and set
+  activePlayer = activePlayer === 1 ? 2 : 1;
+  currentScore = 0;
+  activePlayer !== 1 ? playerOne.classList.add("bg-[#ad6d8c]") : "";
+  playerOne.classList.toggle("bg-[#d4a5b2]");
+  playerTwo.classList.toggle("bg-[#d4a5b2]");
+};
 //Event handler
 rollDiceBtn.addEventListener("click", () => {
   const rollDice = Math.trunc(Math.random() * 6) + 1;
@@ -28,19 +39,91 @@ rollDiceBtn.addEventListener("click", () => {
     currentScore += rollDice;
     document.querySelector(`.current_score--${activePlayer}`).textContent =
       currentScore;
-    console.log(currentScore);
   } else {
-    document.querySelector(`.current_score--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    // document.getElementById(`player--${activePlayer}`).style.backgroundColor =
-    //   "#ad6d8c";
+    switchPlayer();
   }
 });
-newGameBtn.addEventListener("click", () => {
-  dice.classList.add("hidden");
-});
 holdBtn.addEventListener("click", () => {
-  console.log("button clicked");
-  TotalScore += currentScore;
+  //Sum of current score and total score and set it to active player:
+  activePlayer === 1
+    ? (playerOneTotal.textContent = playerOneScore += currentScore)
+    : (playerTwoTotal.textContent = playerTwoScore += currentScore);
+  //Calling switch player function
+  switchPlayer();
+  //Winning condition for player-1
+  if (playerOneScore >= 20) {
+    playerOneName.textContent = "Player - 1 win!";
+    playerOneName.classList.add("bg-amber-300");
+    playerOneName.classList.add("py-3");
+    playerOneName.classList.add("rounded-lg");
+    rollDiceBtn.disable = true;
+    holdBtn.disable = true;
+    playerOneScore = 0;
+  }
+  //Winning condition for player-2
+  if (playerTwoScore >= 20) {
+    playerTwoName.textContent = "Player - 2 win!";
+    playerTwoName.classList.add("bg-amber-300");
+    playerTwoName.classList.add("py-3");
+    playerTwoName.classList.add("rounded-lg");
+    rollDiceBtn.disable = true;
+    holdBtn.disable = true;
+    playerTwoScore = 0;
+  }
 });
+
+newGameBtn.addEventListener("click", () => {
+  activePlayer = 1;
+  dice.classList.add("hidden");
+  document.querySelector(`.current_score--${activePlayer}`).textContent = 0;
+  playerOneTotal.textContent = 0;
+  playerTwoTotal.textContent = 0;
+  playerOneName.textContent = "PLAYER 1";
+  playerOneName.classList.remove("bg-amber-300");
+  playerOneName.classList.remove("py-3");
+  playerOneName.classList.remove("rounded-lg");
+  playerTwoName.textContent = "PLAYER 2";
+  playerTwoName.classList.remove("bg-amber-300");
+  playerTwoName.classList.remove("py-3");
+  playerTwoName.classList.remove("rounded-lg");
+  rollDiceBtn.disable = false;
+  holdBtn.disable = false;
+});
+
+// //Select elements
+// const playerOne = document.getElementById("player--1");
+// const playerTwo = document.getElementById("player--2");
+// const currentScorePOne = document.querySelector(".current_score--1");
+// const currentScorePTwo = document.querySelector(".current_score--2");
+// const rollDiceBtn = document.querySelector(".rollDice");
+// const dice = document.querySelector(".dice");
+
+// //Initial values:
+// let currentScore = 0;
+// let activePlayer = 1;
+// //Event handling
+// rollDiceBtn.addEventListener("click", () => {
+//   //Removing hidden class and open dice
+//   dice.classList.remove("hidden");
+//   //Random number generating to get 1-6 for dice
+//   const rollDice = Math.trunc(Math.random() * 6) + 1;
+//   dice.src = `./dice-${rollDice}.png`;
+
+//   // Add current score and switch player
+//   if (rollDice !== 1) {
+//     //Add current score in active player
+//     document.querySelector(`.current_score--${activePlayer}`).textContent =
+//       currentScore += rollDice;
+//   } else {
+//     //Set current score zero to previous player
+//     document.querySelector(`.current_score--${activePlayer}`).textContent = 0;
+//     //Decide active player and set current score to zero
+//     activePlayer = activePlayer === 1 ? 2 : 1;
+//     currentScore = 0;
+
+//     // Changing color when active player is switched
+//     playerOne.classList.toggle("bg-[#d4a5b2]");
+//     playerTwo.classList.toggle("bg-[#d4a5b2]");
+//     activePlayer !== 1 ? playerOne.classList.add("bg-[#ad6d8c]") : "";
+//   }
+// });
